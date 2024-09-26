588a901b7aa63fef2217a0182b9281a5b8495cc4
Kim Min Sang <sp4593@naver.com>
2024-09-26 17:46:11 +0900

    Route Feature In Progress

--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,61 +1,51 @@
 import { BrowserRouter, Routes, Route, Link, useLocation, Outlet } from "react-router-dom";
-  import Login from "./pages/LoginPage";
-  import About from "./pages/AboutPage";
-  import Goods from "./pages/GoodsPage";
-
 
 function App() {
   return (
     <BrowserRouter>
+    
       <Link to="/"> Home </Link>
       <Link to="/Login"> Login </Link>
       <Link to="/About"> About </Link>
-      <Link to="/Goods"> Goods </Link>
-
-      <Link to="/Three"> Three </Link>
-      {/* 라우트를 감싸줍니다. 차이점을 보기위한 Three*/}
+      <Link to="/Outlet"> Outlet </Link>
 
 
+      {/* 라우트를 감싸줍니다. */}
       <Routes>
       
-        <Route path="" element={<Index />}/>
+        <Route path="/" element={<Index />}/>
         <Route path="/Login" element={<Login name='licat' />}/>
-        <Route path="/Goods" element={<Goods />}/>
-        <Route path="/About" element={<About />}/>
 
-    {/*이 부분 부터 활용법 주의  */}
-        <Route path="/three/*" element={<Outlet />}>
+
+        <Route path="/Outlet/*" element={<Outlet />}>
           <Route path="" element={<ThreeIndex />} />
-         
           <Route path="hojunone/" element={<ThreeOne />} />
           <Route path="hojuntwo/" element={<ThreeTwo />} />
         </Route>
-        {/* 여기까지 */}
-
+        
         <Route path="/blog/:id" element={<Blog />}/>
       
       </Routes>
+
     </BrowserRouter>
   );
 }
 
 function Index(){
-  return <h1>홈페이지</h1>
+  return <h1>hello world0</h1>
 }
 
-
-// function One({name}){
-//   return <h1>{name} world1</h1>
-// }
+function Login({name}){
+  return <h1>{name} world1</h1>
+}
 
 // function Two(){
 //   return <h1>hello world2</h1>
 // }
 
-// function Three(){
+// function Outlet(){
 //   return <h1>hello world3</h1>
 // }
-
 
 function Blog(){
   const location = useLocation()
@@ -63,12 +53,9 @@ function Blog(){
   return <h1>hello Blog</h1>
 }
 
-
-// 이부분 활용법 주의
 function ThreeIndex(){
-  return <h1>여기가 Outlet을 활용한 app.js의 ThreeIndex 글 적는 부분</h1>
+  return <h1>hello ThreeIndex</h1>
 }
-
 
 function ThreeOne(){
   return <h1>hello ThreeOne</h1>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+// src/components/Login.jsx
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginPage = () => (
+  <div>
+    <h1> 로그인</h1>
+    <Link to="/Three">목표 페이지로 이동</Link>
+  </div>
+);
+
+
+export default LoginPage;
+
+
