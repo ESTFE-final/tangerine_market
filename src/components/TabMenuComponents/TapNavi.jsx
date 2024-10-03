24560ea9f907fbf097ba4b84b2a66eabf6744cd5
Kim Min Sang <sp4593@naver.com>
2024-10-03 20:55:46 +0900

    이미지파일 icon 폴더로 변경

---
+++ src/components/TabMenuComponents/TapNavi.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// css 만 넣으면 되것으요 ~ 선생님 ~~~ 아이고 고생하셨어요 자비스 선상님
+
+// SVG 이미지 import
+import iconMain from '../../icons/icon-home.svg';
+import iconAbout from '../../icons/icon-user.svg';
+import iconServices from '../../icons/icon-user.svg';
+import iconContact from '../../icons/icon-user.svg';
+import iconHelp from '../../icons/icon_edit 2.svg';
+
+// 탭 정보를 담은 배열
+const tabs = [
+	{ path: '/', label: '홈', icon: iconMain },
+	{ path: '/about', label: '채팅', icon: iconAbout },
+	{ path: '/services', label: '게시물 목록', icon: iconServices },
+	{ path: '/contact', label: '게시물 작성', icon: iconContact },
+	{ path: '/help', label: '프로필', icon: iconHelp },
+];
+
+function TapNavi() {
+	return (
+		<div className="tap-navi">
+			{tabs.map((tab, index) => (
+				<Link key={index} to={tab.path}>
+					<button>
+						<img src={tab.icon} alt={`${tab.label} icon`} /> {tab.label}
+					</button>
+				</Link>
+			))}
+		</div>
+	);
+}
+
+export default TapNavi;
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import TapNavi from './components/TabMenuComponents/TapNavi.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+	<>
+		<App />
+		<BrowserRouter>
+			<TapNavi />
+		</BrowserRouter>
+	</>
+);
