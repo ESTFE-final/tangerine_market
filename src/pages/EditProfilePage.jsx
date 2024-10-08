a1e611b8807600459845bd15b28c7f865d16e573
jini <ggum920@gmail.com>
2024-10-08 16:44:15 +0900

    ADD / 팔로우리스트, 내프로필편집 파일추가

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -2,11 +2,122 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import Button from '../components/SharedComponents/Button';
+import { NavigationBar, Input } from '../components/SharedComponents/CommonComponents';
+import basicprofileimg from '../icons/basic-profile-img.svg';
+import uploadIcon from '../icons/upload-file.svg';
+
 
 const EditProfilePage = () => (
-	<div>
-		<h1> 프로필 편집 화면입니다</h1>
-	</div>
+	<>
+		<NavigationBar
+			rightButton={
+				<Button size="md" type="button">
+					저장
+				</Button>
+			}
+		/>
+		<h1 className="sr-only"> 프로필 편집 화면입니다</h1>
+		<InnerWMobileFull>
+			<ProfileModifyContent>
+				<ProfileImageContent>
+					<ProfileImage src={basicprofileimg} alt="프로필 없을때" />
+					<ProfileImageLabel htmlFor="profileimg"></ProfileImageLabel>
+					<ProfileImageInput
+						id="profileimg"
+						ype="file"
+						accept="image/*"
+					/>
+				</ProfileImageContent>
+				<ProfileModifyInputBox>
+					<ProfileModifyLabel htmlFor="username">
+						사용자 이름
+					</ProfileModifyLabel>
+					<ProfileModifyInput
+						id="username"
+						type="text"
+						placeholder="2~10자 이내여야 합니다."
+					/>
+				</ProfileModifyInputBox>
+				<ProfileModifyInputBox>
+					<ProfileModifyLabel htmlFor="userid">계정 ID</ProfileModifyLabel>
+					<ProfileModifyInput
+						id="userid"
+						type="text"
+						placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+					/>
+				</ProfileModifyInputBox>
+				<ProfileModifyInputBox>
+					<ProfileModifyLabel htmlFor="userpr">소개</ProfileModifyLabel>
+					<ProfileModifyInput
+						id="userpr"
+						type="text"
+						placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+					/>
+				</ProfileModifyInputBox>
+			</ProfileModifyContent>
+		</InnerWMobileFull>
+	</>
 );
+
+
+const InnerWMobileFull = styled.div`
+	width: 100%;
+	margin: 0 auto;
+	position: relative;
+	padding-bottom: 10rem;
+`
+
+const ProfileModifyContent = styled.div`
+	padding: 0 3.4rem;
+`
+
+const ProfileModifyLabel = styled.label`
+	padding-left: 1.6rem;
+	font-size: 1.8rem;
+	color: #666;
+`
+
+const ProfileModifyInput = styled(Input)`
+	margin-top: 1rem;
+	display: block;
+	padding: 1.8rem 2rem;
+	font-size: 1.6rem;
+`;
+	
+
+const ProfileModifyInputBox = styled.div`
+	margin-bottom: 2.8rem;
+`
+
+const ProfileImageContent = styled.div`
+	position: relative;
+	width: 180px;
+	height: 180px;
+	margin: 0 auto 6rem;
+`
+
+const ProfileImage = styled.img`
+	width: 100%;
+	height: 100%;
+`
+
+const ProfileImageLabel = styled.label`
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	display: block;
+	background: url(${uploadIcon}) no-repeat center;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	cursor: pointer;
+	background-size: 100%;
+`;
+
+const ProfileImageInput = styled.input`
+	display: none;
+`
+
 
 export default EditProfilePage;
--- src/pages/FollowerListPage.jsx
+++ src/pages/FollowerListPage.jsx
@@ -2,11 +2,122 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import Button from '../components/SharedComponents/Button';
+import { NavigationBar } from '../components/SharedComponents/CommonComponents';
 
-const FollowerListPage = () => (
-	<div>
-		<h1> 팔로워 리스트 페이지입니다</h1>
-	</div>
-);
+
+function FollowerListPage() {
+	const images = require.context('../icons', true, /\.(png|jpe?g|svg)$/)
+
+	const followersData = [
+		{
+			name: '댕케',
+			description: '강아지 수제 케이크 | 수제 간식',
+			image: 'img-follower-01.png',
+		},
+		{
+			name: '의류 핸드메이드샵',
+			description: '펫 의류 자체 제작합니다',
+			image: 'img-follower-02.png',
+		},
+		{
+			name: '하루네',
+			image: 'img-follower-03.png',
+		},
+		{
+			name: '냥이집_고양이 용품 판매',
+			description: '고양이 맞춤 용품 판매점',
+			image: 'img-follower-04.png',
+		},
+		{
+			name: '노을이',
+			image: 'img-follower-05.png',
+		},
+		{
+			name: '햄토리몰',
+			description: '햄스터 전용 용품 | 간식 판매 🐹',
+			image: 'img-follower-06.png',
+		},
+		{
+			name: '레빗샵',
+			description: '고토끼 수제 간식, 옷, 용품 등 토순이들...',
+			image: 'img-follower-07.png',
+		},
+	];
+
+
+    return (
+        <>
+            <NavigationBar title="팔로워" />
+            <InnerWMobileFull>
+                <h1 className="sr-only">팔로워 리스트 페이지입니다</h1>
+                <FollowerListContent>
+                    {followersData.map((follower, index) => (
+                        <FollowerListItem key={index}>
+                            <FollowerInfo>
+                                <FollowerImg
+                                    src={images(`./${follower.image}`)}
+                                    alt={follower.name}
+                                />
+                                <FollowerText>
+                                    <FollowerShopName>{follower.name}</FollowerShopName>
+                                    <FollowerShopDesc>{follower.description}</FollowerShopDesc>
+                                </FollowerText>
+                            </FollowerInfo>
+                            <Button size="sm" type="button">
+                                팔로우
+                            </Button>
+                        </FollowerListItem>
+                    ))}
+                </FollowerListContent>
+            </InnerWMobileFull>
+        </>
+    );
+};
+
+
+const InnerWMobileFull = styled.div`
+	width: 100%;
+	margin: 0 auto;
+	position: relative;
+	padding-bottom: 10rem;
+`
+const FollowerInfo = styled.div`
+	display: flex;
+	align-items: flex-start;
+	gap: 1.6rem;
+`
+const FollowerListContent = styled.ul`
+	padding: 0 1.6rem;
+`
+
+const FollowerListItem = styled.li`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	& + & {
+		margin-top: 3.4rem;
+	}
+`;
+
+const FollowerImg =  styled.img`
+	background: var(--gray);
+	width: 72px;
+	height: 72px;
+	overflow: hidden;
+	border-radius: 50%;
+	flex-shrink: 0;
+`
+const FollowerText = styled.div`
+	padding-top: 0.7rem;
+`
+const FollowerShopName = styled.p`
+	font-size: 2rem;
+	margin-bottom: 0.6rem;
+`
+const FollowerShopDesc = styled.p`
+	color: var(--gray-300);
+	font-size: 1.8rem;
+`
 
 export default FollowerListPage;
