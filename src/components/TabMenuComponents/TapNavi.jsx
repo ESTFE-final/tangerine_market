885072cc918267cc36b356f9226408540ee13812
Kim Min Sang <sp4593@naver.com>
2024-10-09 03:12:25 +0900

    상품 등록 페이지 퍼블리싱 수정 css를 스타일드 컴포넌트로 추가 변경

--- src/components/TabMenuComponents/TapNavi.jsx
+++ src/components/TabMenuComponents/TapNavi.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import './TapNavi.css';
+import { Link } from 'react-router-dom';
 
 // SVG 이미지 import
 import iconMain from '../../icons/icon-home.svg';
@@ -9,6 +7,52 @@ import iconAbout from '../../icons/icon-message-circle.svg';
 import iconServices from '../../icons/icon_edit 2.svg';
 import iconContact from '../../icons/icon-edit.svg';
 import iconHelp from '../../icons/icon-user.svg';
+
+const TabNavi = styled.div`
+	display: flex;
+	position: fixed;
+	justify-content: space-around;
+	align-items: center;
+	width: 775px;
+	bottom: 0;
+	max-height: 96px;
+	height: 96px;
+	max-width: 775px;
+	background-color: #ffffff;
+	border-top: 1px solid #e5e5e5;
+`;
+
+const TapNaviButton = styled.button`
+	display: inline-flex;
+	flex-direction: column;
+	justify-content: center;
+	justify-items: center;
+	align-items: center;
+	align-content: space-evenly;
+	padding: 0;
+	margin: 0;
+	width: 107px;
+	max-height: 96px;
+	background: none;
+	border: none;
+	font-family: 'Gmarket Sans TTF';
+	font-size: 20px;
+	color: #767676;
+	font-weight: 900;
+	line-height: 28px;
+	max-width: 126px;
+	word-break: keep-all;
+`;
+
+const TapNaviIcon = styled.img`
+	min-width: 48px;
+	width: 48px;
+	max-height: 48px;
+	margin: auto;
+	padding: auto;
+	justify-content: center;
+	align-items: center;
+`;
 
 // 탭 정보를 담은 배열
 const tabs = [
@@ -21,15 +65,15 @@ const tabs = [
 
 function TapNavi() {
 	return (
-		<div className="tap--navi">
+		<TabNavi>
 			{tabs.map((tab, index) => (
 				<Link key={index} to={tab.path}>
-					<button>
-						<img src={tab.icon} alt={`${tab.label} icon`} /> {tab.label}
-					</button>
+					<TapNaviButton>
+						<TapNaviIcon src={tab.icon} alt={`${tab.label} icon`} /> {tab.label}
+					</TapNaviButton>
 				</Link>
 			))}
-		</div>
+		</TabNavi>
 	);
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import { NavigationBar } from '../components/SharedComponents/CommonComponents';
 import MainBanner from '../components/MainComponents/MainBanner';
 import MainCategory from '../components/MainComponents/MainCategory';
 import MainProductList from '../components/MainComponents/MainProductList';
+
 import { Link } from 'react-router-dom';
 import { BodyContainer } from '../components/SharedComponents/CommonComponents';
 import TapNavi from '../components/TabMenuComponents/TapNavi';
+
+const Tap__Navi = styled.div`
+	display: flex;
+	position: fixed;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 100%;
+	bottom: 0;
+	max-height: 96px;
+	height: 96px;
+	max-width: 775px;
+	background-color: #ffffff;
+`;
+
+const TapNaviButton = styled.button`
+	display: inline-flex;
+	flex-direction: column;
+	justify-content: center;
+	justify-items: center;
+	align-items: center;
+	align-content: space-evenly;
+	padding: 0;
+	margin: 0;
+	width: 107px;
+	max-height: 96px;
+	background: none;
+	border: none;
+	font-family: 'Gmarket Sans TTF';
+	font-size: 20px;
+	color: #767676;
+	font-weight: 900;
+	line-height: 28px;
+	max-width: 126px;
+	word-break: keep-all;
+`;
+
+const TapNaviImg = styled.img`
+	min-width: 48px;
+	width: 48px;
+	max-height: 48px;
+	margin: auto;
+	padding: auto;
+	justify-content: center;
+	align-items: center;
+`;
 
 const MainPage = () => (
 	<>
